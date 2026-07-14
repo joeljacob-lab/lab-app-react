7b5ab4fd4c64c3bc0fc2cd692ddacfe2952f8d05
joel jacob <joelj712@gmail.com>
2026-07-14 20:48:36 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import AddData from "./components/AddData";
-
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavigationBar />
+
       <Routes>
-  
-        <Route path="/add" element={<AddData />} />
-       
+        <Route path="/" element={<AddData />} />
       </Routes>
     </BrowserRouter>
   );
